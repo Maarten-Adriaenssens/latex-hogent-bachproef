67c979a7c5fd5d69cf4e86aaf9161cbdc5992f2f
Maarten-Adriaenssens <maarten.adriaenssens@student.hogent.be>
2025-05-26 11:43:58 +0200

    Fix script.sh by adding missing flags for package installations and correcting comments for clarity

--- POC/script.sh
+++ POC/script.sh
@@ -9,20 +9,20 @@ sudo systemctl status mongod
 
 sudo add-apt-repository ppa:open5gs/latest
 sudo apt update
-sudo apt install open5gs
+sudo apt install -y open5gs
 
 # Download and import the Nodesource GPG key
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-Create deb repository
+
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-Run Update and Install
+#Run Update and Install
 sudo apt update
 sudo apt install nodejs -y
 
-sudo zypper install nodejs8
-
 curl -fsSL https://open5gs.org/open5gs/assets/webui/install | sudo -E bash -
+
+sudo vim /usr/lib/node_modules/open5gs/server/index.js
