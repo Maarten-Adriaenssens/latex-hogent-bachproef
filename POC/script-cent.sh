96533aeb7897e1fb162fe4557da6ecad4c669dd1
Maarten-Adriaenssens <maarten.adriaenssens@student.hogent.be>
2025-05-29 10:24:30 +0200

    Add private network configuration for Vagrant VMs and update scripts for IP changes

--- POC/script-cent.sh
+++ POC/script-cent.sh
@@ -36,7 +36,7 @@ curl -fsSL https://open5gs.org/open5gs/assets/webui/install | sudo -E bash -
 sudo systemctl start open5gs-webui
 sudo systemctl enable open5gs-webui
 
-sudo sed -i "s/'localhost'/'10.0.2.15'/" /usr/lib/node_modules/open5gs/server/index.js # Change localhost
+sudo sed -i "s/'localhost'/'10.0.0.10'/" /usr/lib/node_modules/open5gs/server/index.js # Change localhost
 sudo systemctl daemon-reload
 sudo systemctl restart open5gs-webui.service
 sudo systemctl restart open5gs-webui
@@ -55,8 +55,15 @@ make
 
 #-----------------------------------------------------------
 #Change IP addresses in Open5GS and UERANSIM configuration files
-sudo sed -i '23s/127.0.0.5/10.0.2.15/' /etc/open5gs/amf.yaml # Change AMF IP address
-sudo systemctl restart open5gs-amfd # Restart AMF service to apply changes
-sudo sed -i 's/127.0.0.1/10.0.2.15/' ~/UERANSIM/config/open5gs-ue.yaml # Change UE IP address
-sudo sed -i 's/127.0.0.1/10.0.2.15/' ~/UERANSIM/config/open5gs-gnb.yaml # Change gNB IP address
-sudo sed -i 's/127.0.0.5/10.0.2.15/' ~/UERANSIM/config/open5gs-gnb.yaml # Change gNB AMF IP address
+# sudo sed -i '23s/127.0.0.5/10.0.2.15/' /etc/open5gs/amf.yaml # Change AMF IP address
+# sudo systemctl restart open5gs-amfd # Restart AMF service to apply changes
+# sudo sed -i 's/127.0.0.1/10.0.2.15/' ~/UERANSIM/config/open5gs-ue.yaml # Change UE IP address
+# sudo sed -i 's/127.0.0.1/10.0.2.15/' ~/UERANSIM/config/open5gs-gnb.yaml # Change gNB IP address
+# sudo sed -i 's/127.0.0.5/10.0.2.15/' ~/UERANSIM/config/open5gs-gnb.yaml # Change gNB AMF IP address
+
+#-----------------------------------------------------------
+#NAT Port Forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo systemctl stop ufw
+sudo iptables -I FORWARD 1 -j ACCEPT
--- POC/script-ue.sh
+++ POC/script-ue.sh
@@ -1,19 +1,6 @@
 # Script to install Open5GS and UERANSIM on Ubuntu 22.04
 # Device: Member / UE
 #-----------------------------------------------------------
-# Download and import the Nodesource GPG key
-sudo apt update
-sudo apt install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-#Run Update and Install
-sudo apt update
-sudo apt install nodejs -y
-
-#-----------------------------------------------------------
 #Installation of UERANSIM
 git clone https://github.com/aligungr/UERANSIM
 sudo apt install -y cmake
